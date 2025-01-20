b8905326be0b9bcff7e0b26f1722a0c28926bcbe
MNAutriz <mgautriz@up.edu.ph>
2025-01-20 23:39:58 +0800

    Added the component for changing the display menu

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './HomePage.css'
-import { assets } from '../../assets/frontend_assets/assets'
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/Exploremenu'
+import { useState } from 'react'
+
 
 const HomePage = () => {
+    const [category, setCategory] = useState('All') 
+
+
   return (
     <div className='home-container'>
         <Header />
+        <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
