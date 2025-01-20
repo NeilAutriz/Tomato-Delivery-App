0f03144a738e198a6d19f1be799c023e7d8d3010
MNAutriz <mgautriz@up.edu.ph>
2025-01-20 22:21:47 +0800

    Added the header for the homepage and the correct routing set-ups

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/Home/HomePage'
+import CartPage from './pages/Cart/CartPage'
+import PlaceOrderPage from './pages/PlaceOrder/PlaceOrderPage'
+
 
 function App() {
 
   return (
     <div className='main-container'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/order' element={<PlaceOrderPage />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './HomePage.css'
+import { assets } from '../../assets/frontend_assets/assets'
+import Header from '../../components/Header/Header'
+
+const HomePage = () => {
+  return (
+    <div className='home-container'>
+        <Header />
+    </div>
+  )
+}
+
+export default HomePage
